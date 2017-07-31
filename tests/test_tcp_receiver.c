52ec00c6d8bff4acdcf543207d1051e384bc3a4d
Ubuntu <ubuntu@ip-172-31-30-57.ec2.internal>
2017-07-31 10:48:35 +0000

    tcp total time

--- tests/test_tcp_receiver.c
+++ tests/test_tcp_receiver.c
@@ -121,14 +121,14 @@ int main(int argc, char *argv[])
     {
     	sleep(5);
 
-    	printf("\n\n nb msg received: %d \ntotal time received %d \n", count_msg, (time_total - time_start)/ CLOCKS_PER_SEC);
-    	printf("avg:  %f\n", (time_total/(float)count_msg)/ CLOCKS_PER_SEC);
+    	printf("\n\n nb msg received: %d \ntotal time received %lf \n", count_msg, ((double)time_total)/ CLOCKS_PER_SEC);
+    	printf("avg:  %f\n", (time_total/(double)count_msg)/ CLOCKS_PER_SEC);
     }
 
 	sleep(1);
 	printf("Total: ");
-	printf("\n\n nb msg received: %d \ntotal time received %d \n", count_msg, (time_total - time_start)/ CLOCKS_PER_SEC);
-	printf("avg:  %f\n", (time_total/(float)count_msg)/ CLOCKS_PER_SEC);
+	printf("\n\n nb msg received: %d \ntotal time received %lf \n", count_msg, ((double)time_total)/ CLOCKS_PER_SEC);
+	printf("avg:  %lf\n", (time_total/(double)count_msg)/ CLOCKS_PER_SEC);
 
 	return 0;
 }
